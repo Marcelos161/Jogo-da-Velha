4918922516a8b2a3c3679bfd7d0e3847f2860229
Marcelo Diogo <marcelos.diogos@outlook.com>
2023-03-09 22:21:36 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           edite o <code>src/app.js</code> e salve para reload
-          Vo manda todo mundo pro capeta desgraca
+          Vo manda todo mundo pro capeta desgraca fdp
         </p>
         <a
           className="App-link"
