0212c49f72d39ab44a49b3b9512ed187dc71998a
Marcelo Diogo <marcelos.diogos@outlook.com>
2023-03-19 17:59:13 -0300

    Style do Head refeito

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,16 +2,15 @@ import React from "react";
 import "./styles.css"
 import Sobre from "../../objects/sobre";
 import Union from "../../objects/union";
-import Logo from "../../objects/logo"
+import Logo from "../../objects/logo";
 
 const Header = () => {
-    return (<div className="header">
-        <>
-            <Logo />
-            <Sobre />
-            <Union />
-        </>
-    </div>)
-}
+    return (<header className="header">
+        
+                <Logo />
+                <Sobre />
+                <Union />
+            </header>)
+};
 
-export default Header
+export default Header;
