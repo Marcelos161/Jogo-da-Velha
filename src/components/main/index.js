f7ce55329b42419323e4c8f355add727b90de8c7
Marcelo Diogo <marcelos.diogos@outlook.com>
2023-03-19 21:48:27 -0300

    Inicio da adicao do X e circulo

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Tab from "../../objects/tab-jogo-da-velha";
+import X from "../../objects/GameX"
+import './styles.css'
 
 const Main = () => {
-    return (<Tab />)
+    return (<div className="Container-tab"><Tab /> <X /></div>)
 }
 
 export default Main
---
+++ src/objects/GameX/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './styles.css'
+
+const X = () => {
+    return  (<span className="x">X</span>)
+};
+
+export default X;
